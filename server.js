96131462afef290f0d930b2015119bc3de44ab27
brandon-stewart-rgb <brandon@pixeltree.us>
2021-08-27 16:56:52 -0600

    feature parties

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const inputCheck = require('./utils/inputCheck');
+const e = require('express');
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -110,19 +111,86 @@ app.post('/api/candidate', ({ body }, res)=>{
     });
 });
 
+// update a candidate's party
+app.put('/api/candidate/:id', (req, res)=>{
+    const errors = inputCheck(req.body, 'party_id');
 
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                 VALUES (?,?,?,?)`;
-//                 // array must match placeholders
-// const params = [1, 'Ronald', 'Firbank', 1];
+    if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+    }
 
-// db.query(sql, params,(err, result) => {
-//     if(err){
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+    const sql = `UPDATE candidates SET party_id = ?
+                WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result)=>{
+        if(err) {
+            res.status(400).json({ error: err.message });
+            // check if record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
 
+
+app.get('/api/parties', (req, res)=>{
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows)=>{
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+app.get('/api/party/:id', (req, res)=>{
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row)=>{
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+app.delete('/api/party/:id', (req, res)=>{
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query( sql, params, (err, result)=>{
+        if (err) {
+            res.status(400).json({ error: res.message });
+            // checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            })
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 
 // default response for any other request (not found) make sure it's the last route
