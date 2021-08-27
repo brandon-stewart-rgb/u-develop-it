90fcdd9a43017ab63ae3a40125f271b719339662
brandon-stewart-rgb <brandon@pixeltree.us>
2021-08-27 16:12:20 -0600

    select routes

--- schema.sql
+++ schema.sql
@@ -1,36 +1,27 @@
 CREATE DATABASE election;
 
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
 USE election;
 
-CREATE TABLE candidates (
+CREATE TABLE parties (
 id INT AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-industry_connected BOOLEAN NOT NULL
+name VARCHAR(50) NOT NULL,
+description TEXT
 );
 
 
+CREATE TABLE candidates (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
 
-SELECT * FROM candidates;
 
-# retrieve only specific column values
-SELECT first_name, last_name FROM candidates;
-
-# find all candidates that are industry_connected
-SELECT first_name, industry_connected
-FROM candidates
-WHERE industry_connected =1;  # where industry connected is true 0 for false, 1 for true boolien values
-
-SELECT first_name, last_name, industry_connected
-FROM candidates
-WHERE id = 5; # select a row with the primary key of 5
-
-UPDATE candidates 
-SET industry_connected =1
-WHERE id = 3;
-
-DELETE FROM candidates
-WHERE first_name = 'Montague';
 
 
 
